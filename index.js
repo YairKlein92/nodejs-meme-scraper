861741317a13173a8baecb566a0b5384af3cf888
Csaba Klein <sympla@gmail.com>
2023-01-11 17:36:37 +0100

    Put all the links in any array
    filter through and keep all with <api> in it

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import fetch from 'node-fetch';
 
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
-let htmlContent = '';
 
 const response = await fetch(memeUrl);
 const body = await response.text();
 
-console.log(body);
+console.log(typeof body);
+
+const expression =
+  /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
+//const exp = '^https://api.memegen.link/images$'
+let arrayLinks = [];
+let matches = body.match(expression);
+for (let match in matches) {
+  //  console.log(matches[match]);
+  arrayLinks.push(matches);
+}
+console.log(arrayLinks);
--- tryout.js
+++ tryout.js
@@ -33,3 +33,33 @@ getContent(memeUrl)
 
 htmlContent = await getContent(memeUrl);
 console.log(htmlContent);
+
+let imageTags = document.getElementsByTagName('img'); // Returns array of <img> DOM nodes
+const sources = [];
+for (let i in imageTags) {
+  let src = imageTags[i].src;
+  sources.push(src);
+}
+
+import fetch from 'node-fetch';
+
+const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+const response = await fetch(memeUrl);
+const body = await response.text();
+
+console.log(body);
+
+import request from 'request';
+import cheerio from 'cheerio';
+
+const URL_TO_PARSE = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+// Make a request to get the HTML of the page
+request(URL_TO_PARSE, (err, response, body) => {
+  if (err) throw new Error('Something went wrong');
+  // Load the HTML into cheerio's DOM
+  const $ = cheerio.load(body);
+  // Print the text nodes of the <table> in the HTML
+  console.log($('img').text());
+});
