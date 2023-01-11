40fcf7bad8a79ba2336b14f809559b62e987be52
Csaba Klein <sympla@gmail.com>
2023-01-11 21:04:40 +0100

    Extract the URL of first 10 pictures to an array

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 const response = await fetch(memeUrl);
 const body = await response.text();
 
-console.log(typeof body);
+//console.log(body);
 
 const expression =
   /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
-//const exp = '^https://api.memegen.link/images$'
+//  const exp = '^https://api.memegen.link/images$'
 let arrayLinks = [];
 let matches = body.match(expression);
 for (let match in matches) {
-  //  console.log(matches[match]);
-  arrayLinks.push(matches);
+  arrayLinks.push(matches[match]);
 }
-console.log(arrayLinks);
+//  console.log(arrayLinks); //this is the array containing all the links
+
+//  getting IMG links out of the link with filter
+let newArray = arrayLinks.filter((name) => {
+  return !name.includes('memecomplete.com');
+});
+//  console.log(newArray);
+newArray = newArray.slice(4, 14);
+console.log(newArray);
