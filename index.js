a2db3352a5e63378702c90a2c69ebc8d5d3c6aa0
Csaba Klein <sympla@gmail.com>
2023-01-12 09:57:10 +0100

    Create function that deletes our memes folder
    If we have a memes folder, it might contains pics so it is save
    to delete it at the beginning of our program.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+import fs from 'fs'; //sending downloaded image to folder
+import https from 'https'; //downloading image
 import fetch from 'node-fetch';
 
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
 const response = await fetch(memeUrl);
 const body = await response.text();
+const memeFolder = './memes';
 
 //console.log(body);
 
@@ -22,5 +25,45 @@ let newArray = arrayLinks.filter((name) => {
   return !name.includes('memecomplete.com');
 });
 //  console.log(newArray);
-newArray = newArray.slice(4, 14);
-console.log(newArray);
+let links = newArray.slice(4, 14);
+console.log(links);
+
+//looping through the links in the array
+///for (let link in links) {
+//}
+
+function downloadImage(url, filepath) {
+  return new Promise((resolve, reject) => {
+    client.get(url, (res) => {
+      if (res.statusCode === 200) {
+        res
+          .pipe(fs.createWriteStream(filepath))
+          .on('error', reject)
+          .once('close', () => resolve(filepath));
+      } else {
+        // Consume response data to free up memory
+        res.resume();
+        reject(
+          new Error(`Request Failed With a Status Code: ${res.statusCode}`),
+        );
+      }
+    });
+  });
+}
+
+try {
+  if (fs.existsSync(memeFolder)) {
+    console.log('Directory exists.');
+    fs.rmdir(memeFolder, () => {
+      console.log('Folder Deleted!');
+    });
+    //we need to delete it
+  } else {
+    console.log('Directory does not exist.');
+  }
+} catch (e) {
+  console.log('An error occurred.');
+}
+//for (let link in links) {
+//downloadImage(links[link]);
+//}
