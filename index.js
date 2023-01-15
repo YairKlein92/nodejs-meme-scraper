106e109ebd2dc28ad8d05b75b499c5660d95a851
Csaba Klein <sympla@gmail.com>
2023-01-15 21:29:31 +0100

    Final version

--- index.js
+++ index.js
@@ -1,91 +1,66 @@
-import cliProgress from 'cli-progress';
-import fs from 'fs'; //  sending downloaded image to folder
-import client from 'https'; //  downloading image
+import fs from 'node:fs'; //  sending downloaded image to folder
+import client from 'node:https'; //  downloading image
+import path from 'node:path';
 import fetch from 'node-fetch';
 
-const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-const bar2 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const memeUrl = 'https://memegen-link-examples-upleveled.netlify.app/'; //  website with the memes
-
 const response = await fetch(memeUrl);
 const body = await response.text(); // body of the html
-const memeFolder = './memes';
-
-//  console.log(body);
-
+const memeFolder = 'C:/Users/sympl/projects/nodejs-meme-scraper/memes';
+// regular expression for https://
 const expression =
-  /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi; // regular expression for links
-//  const exp = '^https://api.memegen.link/images$'
+  /(https?:\/\/(?:www\.|(?!www))[^\s\\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
 
 // creating an array of all the parts that start with    (our regular expression)
 const arrayLinks = [];
 const matches = body.match(expression);
+
 for (const match in matches) {
   arrayLinks.push(matches[match]);
 }
 
-//  getting IMG links out of the link with filter -> I need to download them
+//  getting the right links out of all the links with filtering them
 const newArray = arrayLinks.filter((name) => {
   return !name.includes('memecomplete.com');
 });
 
-//  Getting only 10 links
+//  Getting only 10 links, the first 4 were irrelevant links
 const links = newArray.slice(4, 14);
 
-//  looping through the links in the array
-//  for (let link in links) {
-//  }
-
+// function that clears the folder if it is not already empty
 function manageFolder() {
-  if (fs.existsSync(memeFolder)) {
-    //  rmSync instead of rmdirSync bc I delete a non-empty folder, and have to force it. it makes sure to delete the folder at the beginning of the program even if its not empty
-    console.log('\nDeleting existing folder..');
-    fs.rmSync(memeFolder, { recursive: true, force: true });
-    console.log('Folder deleted');
-    setTimeout(() => {
-      console.log('Creating a folder for the pictures..');
-      fs.mkdirSync(memeFolder);
-      console.log('Folder created.');
-    }, 600); //  fs.rmSync(dir, { recursive: true, force: true });
-  } else {
-    fs.mkdirSync(memeFolder);
-  }
+  console.log('Memes folder is being created/cleared');
+  fs.readdir(memeFolder, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(memeFolder, file), (error) => {
+        if (error) throw error;
+      });
+    }
+  });
 }
 
+// function for downloading pictures
 function downloadImage(url, filepath) {
   client.get(url, (res) => {
     res.pipe(fs.createWriteStream(filepath));
   });
 }
 
-// The Actual Program
-// Deleting directory at the beginning of the program
-//bar1.start(100, 0);
-bar1.start(100, 0);
-manageFolder();
-
-// Downloading The pictures and sending them to the right folder
-//  Combining downloadImage and looping through.
+// Function that runs the program
 // Number() makes sure that it wont be a string concatenation
-// for (const link in links) {
-//   setTimeout(() => {
-//     console.log(`Downloading ${Number(link) + 1}. images is in process..`);
-//     downloadImage(links[link], `./memes/0${Number(link) + 1}.jpg`);
-//   }, 3500);
-// }
 function startProgram() {
   for (const link in links) {
     setTimeout(() => {
       console.log(`Downloading ${Number(link) + 1}. image is in process..`);
       downloadImage(links[link], `./memes/0${Number(link) + 1}.jpg`);
-    }, 1500);
+    }, 3500);
   }
 }
-startProgram();
 
+manageFolder();
+startProgram();
 setTimeout(() => {
   console.log('Pictures are Downloaded.');
-}, 2500);
-setTimeout(() => {
-  bar2.start(100, 100);
-}, 2500);
+}, 5500);
